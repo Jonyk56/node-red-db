e0436c9a5145b994918fabf26a32b7701f2429c3
Jonyk56 <LestusJ@gmail.com>
2020-09-01 14:26:55 +0000

    more updates

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
 const util = require("util")
 
@@ -8,25 +8,17 @@ module.exports = class {
      * @param {string} PreloadedDatabase Database to preload (Must be file)
      */
     constructor(PreloadedDatabase = path.join(`${process.cwd()}`,`db.nrdb`)){
-        Object.defineProperty(this, "data", { value: "", isWritable: true});
+        /**Create "data" object, to be used later */
+        Object.defineProperty(this, "data", { value: {}, isWritable: true});
+        /**check that the path is proper */
         if (!PreloadedDatabase.startsWith(process.cwd())){
             throw new Error("Database path must begin with the current executing path ( process.cwd )");
         }
-        
-        
-    }
-    /**
-     * 
-     * @param {string} FilePath path to the file
-     */
-    static mkFile(FilePath){
-        return fs.writeFileSync(FilePath, "{ }", {encoding:"utf8",flag:"wx+"});
-    }
-    /**
-     * 
-     * @param {string} FilePath path to the file
-     */
-    static ReadFile(FilePath){
-        return fs.readFileSync(FilePath, { encoding:"utf8", flag:"r"});
+        /**Make sure the file at leasts exists */
+        fs.ensureFileSync(PreloadedDatabase);
+        let FileData = fs.readJSONSync(PreloadedDatabase, {throws:false });
+        if (FileData === null){
+            
+        }
     }
 }
