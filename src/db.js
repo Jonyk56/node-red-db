833d234e24c8ec660f1c97642ca5281ec87a78ce
Jonyk56 <LestusJ@gmail.com>
2020-09-01 13:26:47 +0000

    Add some of the basis stuff ( the encoder isn't added yet, this thing is gonna be way better than you think!

--- src/db.js
+++ src/db.js
@@ -1,11 +1,19 @@
+const fs = require("fs");
+const path = require("path")
 /**@class db */
 module.exports = class {
     /**
      * 
      * @param {string} PreloadedDatabase Database to preload (Must be file)
      */
-    constructor(PreloadedDatabase = `${process.cwd()}/db.nrdb`){
+    constructor(PreloadedDatabase = path.join(`${process.cwd()}`,`db.nrdb`)){
         Object.defineProperty(this, "data", { value: "", isWritable: true});
-        
+        if (!PreloadedDatabase.startsWith(process.cwd())){
+            throw new Error("Database path must begin with the current executing path ( process.cwd )");
+        }
+        let FileStats = fs.statSync(PreloadedDatabase);
+        if (!FileStats.isFile()){
+            throw new Error("The path provided does not lead to a file, ")
+        }
     }
 }
