67ea3fc9f7983d7bfdb36ad7c99e18a2e526bdd7
Jonyk56 <LestusJ@gmail.com>
2020-09-01 17:26:28 +0000

    add more to the database

--- src/db.js
+++ src/db.js
@@ -1,24 +1,88 @@
 const fs = require("fs-extra");
 const path = require("path");
-const util = require("util")
+const util = require("util");
+const Serializer = require("./serializer")
+class db{
 
-module.exports = class {
+
     /**
      * 
      * @param {string} PreloadedDatabase Database to preload (Must be file)
      */
-    constructor(PreloadedDatabase = path.join(`${process.cwd()}`,`db.nrdb`)){
+    constructor(PreloadedDatabase = path.join(`${process.cwd()}`, `db.nrdb`)) {
         /**Create "data" object, to be used later */
-        Object.defineProperty(this, "data", { value: {}, isWritable: true});
+        Object.defineProperty(this, "Database", { value: {}, writable: true });
         /**check that the path is proper */
-        if (!PreloadedDatabase.startsWith(process.cwd())){
+        if (!PreloadedDatabase.startsWith(process.cwd())) {
             throw new Error("Database path must begin with the current executing path ( process.cwd )");
         }
+
+        Object.defineProperty(this, "DatabasePath", { value: PreloadedDatabase, writable: false });
         /**Make sure the file at leasts exists */
         fs.ensureFileSync(PreloadedDatabase);
-        let FileData = fs.readJSONSync(PreloadedDatabase, {throws:false });
-        if (FileData === null){
-            
-        }
+        let RenderedFileData;
+        fs.readJSON(PreloadedDatabase, { throws: false }).then(FileData => {
+            if (FileData === null) { FileData = {} }
+            RenderedFileData = FileData;
+        });
+        this.Database = RenderedFileData;
+
+    }
+    SaveFile() {
+        fs.writeJSON(this.DatabasePath, this.Database, { throws: false }).then(() => {
+            return true;
+        });
+    }
+    /**
+     * 
+     * @param {string} TableName name of table
+     */
+    CreateTable(TableName) {
+        this.Database[TableName] = [];
+        this.SaveFile();
+    }
+    /**
+     * 
+     * @param {string} TableName 
+     */
+    DeleteTable(TableName) {
+        delete this.Database[TableName];
+        this.SaveFile();
+    }
+    /**
+     * 
+     * @param {string} TableName 
+     */
+    SelectTable(TableName) {
+        return new Table(this, TableName);
     }
 }
+
+/**@class Table */
+class Table {
+
+    /**@this {Array<any>} table the table*/
+    /**
+     * 
+     * @param {db} Parent 
+     * @param {string} TableName 
+     */
+    constructor(Parent, TableName) {
+        this.table = Parent.Database[TableName];
+
+    }
+    /**
+     * 
+     * @param {any} search search function
+     */
+    find(search) {
+        return this.table.filter(search)
+        
+    }
+}
+
+class TableSearchResult{
+    
+}
+
+module.exports = db
